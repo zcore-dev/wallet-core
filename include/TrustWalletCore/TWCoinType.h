428dd36e1a4e408cbab4cc4be23d012752fe2f08
root <root@electrum.zcore.cash>
2020-03-28 19:12:49 +0000

    add zcore coin type

--- include/TrustWalletCore/TWCoinType.h
+++ include/TrustWalletCore/TWCoinType.h
@@ -46,6 +46,7 @@ enum TWCoinType {
     TWCoinTypeKin = 2017,
     TWCoinTypeLitecoin = 2,
     TWCoinTypeMonacoin = 22,
+    TWCoinTypeZCore = 428,
     TWCoinTypeNebulas = 2718,
     TWCoinTypeNULS = 8964,
     TWCoinTypeNano = 165,
